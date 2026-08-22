3dffe7953878d78cb8e13b8e06c4c1be842d2780
EowynC <kenzo.warrens@hotmail.com>
2026-08-22 10:17:31 +0200

    Changed mainlayout css

--- src/components/layout/MainLayout.vue
+++ src/components/layout/MainLayout.vue
@@ -1,8 +1,33 @@
+<style>
+    .app-layout {
+        width: 100%;
+        display: flex;
+        flex-flow: column;
+    }
+
+    .app-header {
+        width: 100%;
+        display: flex;
+        flex-flow: row;
+        align-items: center;
+        gap: 2rem;
+        margin: 2rem;
+    }
+
+    .app-nav {
+        width: 100%;
+        display: flex;
+        flex-flow: row;
+        justify-content: flex-start;
+        align-items: center;
+        gap: 1rem;
+    }
+</style>
 <template>
     <div class="app-layout">
         <header class="app-header">
-            <h1>Drinkr</h1>
-            <nav>
+            <h2>Drinkr</h2>
+            <nav class="app-nav">
                 <RouterLink to="/bar">
                     Bar
                 </RouterLink>
